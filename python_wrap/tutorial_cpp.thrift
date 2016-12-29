05f0140069fdce4748f2f3a6d6f0f3b22f607f02
xieqy <xieqinye@unisound.com>
2016-12-29 14:26:18 -0500

    add thrift warp

---
+++ python_wrap/tutorial_cpp.thrift
@@ -0,0 +1,30 @@
+namespace cpp tutorial
+namespace d tutorial
+namespace java tutorial
+namespace php tutorial
+namespace perl tutorial
+namespace haxe tutorial
+
+/**
+ * Structs are the basic complex data structures. They are comprised of fields
+ * which each have an integer identifier, a type, a symbolic name, and an
+ * optional default value.
+ *
+ * Fields can be declared "optional", which ensures they will not be included
+ * in the serialized output if they aren't set.  Note that this requires some
+ * manual management in some languages.
+ */
+struct Work {
+  1: i32 id = 0,
+  2: string input_sent,
+  3: string translate_ret,
+}
+
+/**
+ * Ahh, now onto the cool part, defining a service. Services just need a name
+ * and can optionally inherit from another service using the extends keyword.
+ */
+service SMT {
+   Work translate(1:i32 id, 2:string sent),
+   oneway void init()
+}
