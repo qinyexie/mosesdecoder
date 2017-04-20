f23911214865c1ef5bcc358c7006b9e6744095ca
root <root@localhost.localdomain>
2017-04-20 01:12:13 +0000

    thrift only

--- python_wrap/build_python.sh
+++
@@ -1,2 +0,0 @@
-g++ PythonInterface.cpp -o translate.so -shared -I /work/pybind11/include -I ../ -I ../opt/include -L ./lib -L ../opt/lib -lmoses -lboost_thread -lboost_system -lpthread -std=c++11 -fPIC -DMAX_NUM_FACTORS=4 -DWITH_THREADS `python-config --cflags --ldflags`
-# g++ -shared -I /work/pybind11/include -I ../ -L ./lib -L ../opt/lib -lmoses -lboost_thread -std=c++11 -DMAX_NUM_FACTORS=4 -DWITH_THREADS `python-config --cflags --ldflags` PythonInterface.cpp -o translate.so
